989a5a4cb3ae952f903edcb13b0fa45dd92731b5
Dave Lambley <davel@state51.co.uk>
2011-11-29 18:03:02 +0000

    Grab a file object too.

--- scripts/download_as_yaml.pl
+++ scripts/download_as_yaml.pl
@@ -6,5 +6,6 @@ use warnings;
 use state51::APIClient::Media::v1;
 use YAML qw/ DumpFile /;
 
-DumpFile("nmc_example.yaml", state51::APIClient::Media::v1->instance->loader->GET(["licensor/577538050016/release/upc/5023363017725"]));
+DumpFile("nmc_example.yaml", state51::APIClient::Media::v1->instance->GET(["licensor/577538050016/release/upc/5023363017725"]));
+DumpFile("nmc_example_file.yaml", state51::APIClient::Media::v1->instance->GET(["filestore/478181073"]));
 
