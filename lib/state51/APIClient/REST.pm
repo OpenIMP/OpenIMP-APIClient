522915084622dc984a44e3fe4b42b64a21de7db6
Dave Lambley <davel@state51.co.uk>
2011-11-30 12:08:33 +0000

    Fix up tests and documentation.

--- lib/state51/APIClient/REST.pm
+++ lib/state51/APIClient/REST.pm
@@ -6,7 +6,7 @@ use MooseX::Types::Moose qw/ Bool /;
 has __REPRESENTATION__ => (
     is => 'ro',
     isa => 'Str',
-    documentation => 'The URI of the object represented.',
+    documentation => 'The relative URI of the object represented.',
     writer => '_set___REPRESENTATION__',
 );
 
@@ -24,6 +24,7 @@ has __LOADED__ => (
     is => 'rw',
     isa => Bool,
     default => 0,
+    documentation => "Becomes true when this object has been loaded on the fly after a unset attribute was read."
 );
 
 sub _load {
@@ -32,6 +33,7 @@ sub _load {
     warn "inflating from ".$self->__REPRESENTATION__;
     my $hash = $self->_client->GET([$self->__REPRESENTATION__]);
     $self->_loader->load_class($hash, $self, $self->_client);
+    $self->__LOADED__(1);
     return;
 }
 
