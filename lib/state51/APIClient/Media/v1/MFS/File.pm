f25f010803556b32426a68151b2082d78b22c649
Dave Lambley <davel@state51.co.uk>
2011-11-30 14:02:45 +0000

    Stub class to add methods into.

---
+++ lib/state51/APIClient/Media/v1/MFS/File.pm
@@ -0,0 +1,10 @@
+package state51::APIClient::Media::v1::MFS::File;
+
+use Moose;
+
+extends qw/ state51::APIClient::REST /;
+
+# Invoked by state51::APIClient::Loader when exploding all the attributes from yaml.
+# __PACKAGE__->meta->make_immutable;
+
+1;
