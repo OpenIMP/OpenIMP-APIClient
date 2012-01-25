c2c7921c99d9011cbd6134b58c5a6ade8c1b2cea
Dave Lambley <davel@state51.co.uk>
2012-01-25 11:27:31 +0000

    Less file leaking.

--- hudson_test.sh
+++ hudson_test.sh
@@ -2,12 +2,14 @@
 
 set -x
 
-export TMPDIR=/space/hudson/tmpfs
+export TMPDIR=/space/hudson/tmpfs/api-client-$$
 export FILE_CACHE_DIR=/space/hudson/tmpfs
 export STATE51_PLATFORM_TEST_DATA=/space/platform-test-data/
 MEMCACHE_SERVERS=
 MOGILE_TRACKER=
 VARNISH_HOSTS=
+
+mkdir $TMPDIR
 
 echo $WORKSPACE
 
@@ -15,3 +17,4 @@ perl Makefile.PL --skipdeps
 make
 
 prove -Ilib --timer -r --formatter TAP::Formatter::JUnit t/ > junit_output.xml
+rm -rf $TMPDIR
